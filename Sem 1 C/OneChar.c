d81e4ce2fd45b5cf72d0ecd584223ef016131532
Subham Shrestha <138362124+Subham-Shrestha@users.noreply.github.com>
2025-12-27 19:52:29 +0545

    Delete Sem 1 C directory

--- Sem 1 C/OneChar.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    char character;
-    printf("Enter a character: ");
-    scanf("%c", &character);
-    printf("The character you entered is: %c\n", character);
-    return 0;
-}
