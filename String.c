90e8d040fa1bfd5549c480ac8609b98c160fd947
Subham-Shrestha <subham.shrestha008@gmail.com>
2026-01-13 08:13:12 +0545

    Update all code.

--- String.c
+++ String.c
@@ -31,17 +31,47 @@ int main(){
 } */
 
 // WAP to input name and print welcome "name" without using %s.
-#include<stdio.h>
+/* #include<stdio.h>
 int main(){
-    char name[50] = "Ram";
+    char name[50] = "Subham";
     int i = 0;
     printf("Welcome ");
     while(name[i] != '\0'){
         printf("%c", name[i]);
         i++;
     }
-    printf("\n");
+    return 0;
+} */
+
+// WAP to check the charater numbers in a string.
+/* #include<stdio.h>
+int main(){
+    char string[100];
+    int i = 0, count = 0;
+    printf("Enter a name: ");
+    scanf("%s", &string);  //& shows error here but works fine.
+    while(string[i] != '\0'){
+        count++;
+        i++;
+    }
+    printf("Number of letter is: %d\n", count);
+    return 0;
+} */
+
+// WAP to create emoji code using emoji.
+#include<stdio.h>
+int main(){
+    char emoji[] = "\U0001F600";
+    printf("Emoji code is: %s\n", emoji);
     return 0;
 }
 
-// 
+// WAP to input full name and print welcome "full name".
+/* #include<stdio.h>
+int main(){
+    char name[100];
+    printf("Enter your full name: ");
+    scanf("%s", &name);
+    printf("Welcome %s\n", name);
+    return 0;
+} */
