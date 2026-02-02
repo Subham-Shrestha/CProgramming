ccb8d6e4265a245437cb4a157bd6575d3fe6326e
Subham-Shrestha <subham.shrestha008@gmail.com>
2026-02-02 08:43:15 +0545

    chore: commit Structure.c - pushed by assistant

---
+++ Structure.c
@@ -0,0 +1,37 @@
+// WAP to store name, roll, age of a student in a structure and display it.
+#include<stdio.h>
+#include<string.h>
+/* struct Student{
+    int roll;
+    char name[50];
+    int age;
+}; */
+/* int main(){
+    struct Student std; // = {05, "Sandip", 19}; can be also used to initialize but not the best approach.
+    std.roll = 5;
+    strcpy(std.name, "Sandip");
+    std.age = 19; */
+
+    /* printf("Enter name : ");
+    scanf("%s", std.name);
+    printf("Enter roll : ");
+    scanf("%d", &std.roll);
+    printf("Enter age : ");
+    scanf("%d", &std.age); */
+
+    /* printf("Detail of Student : \n");
+    printf("Roll : %d\n", std.roll);
+    printf("Name : %s\n", std.name);
+    printf("Age : %d\n", std.age);  */
+
+    // Book System.
+    struct Book{
+        int id;
+        char name[100];
+        float price;
+    };
+    int main(){
+        
+    }
+    
+    // return 0;
