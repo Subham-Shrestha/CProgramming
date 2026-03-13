92c708929b28aad7c9989dbc17fe72ea8ed29d1c
Subham-Shrestha <subham.shrestha008@gmail.com>
2026-03-13 07:06:01 +0545

    Updated

--- ProjectW.c
+++ ProjectW.c
@@ -41,36 +41,63 @@ int main()
     fclose(fp); */
 
     fp = fopen("studentData.txt", "r");
-    // checking if file exists or not
-    if (fp == NULL)
+    // // checking if file exists or not
+    // if (fp == NULL)
+    // {
+    //     printf("Unable to find file.\n");
+    // }
+    // else
+    // {
+    //     char line[256];
+    //     printf("Details are : \n");
+    //     i = 0;
+    //     while (i < 10 && fgets(line, sizeof(line), fp))
+    //     {
+    //         // Expected format: roll\tname\taddress\tgender\tage\n
+    //         if (sscanf(line, "%d\t%49[^	]\t%49[^	]\t %c\t%d", &std[i].roll, std[i].name, std[i].address, &std[i].gender, &std[i].age) == 5)
+    //         {
+    //             printf("Roll : %d \tName : %s \tAddress : %s \tGender : %c \tAge : %d \n", std[i].roll, std[i].name, std[i].address, std[i].gender, std[i].age);
+    //             i++;
+    //         }
+    //         else
+    //         {
+    //             printf("Skipping malformed line: %s", line);
+    //         }
+    //     }
+    //     if (i == 0)
+    //     {
+    //         printf("No valid records found.\n");
+    //     }
+    // }
+    int max_age=std[0].age;
+    int min_age=std[0].age;
+    // comparing and replacing the max and min age
+    for (i=0; i<10;i++)
     {
-        printf("Unable to find file.\n");
-    }
-    else
-    {
-        char line[256];
-        printf("Details are :\n");
-        i = 0;
-        while (i < 10 && fgets(line, sizeof(line), fp))
+        if (max_age < std[i].age)
         {
-            // each line should be: roll\tname\taddress\tgender\tage\n
-            if (sscanf(line, "%d\t%49[^	]\t%49[^	]\t %c\t%d", &std[i].roll,
-                       std[i].name, std[i].address, &std[i].gender, &std[i].age) == 5)
-            {
-                printf("Roll : %d\tName : %s\tAddress : %s\tGender : %c\tAge : %d\n",
-                       std[i].roll, std[i].name, std[i].address, std[i].gender, std[i].age);
-                i++;
-            }
-            else
-            {
-                printf("Skipping malformed line: %s", line);
-            }
+            max_age = std[i].age;
         }
-        if (i == 0)
+        if (min_age > std[i].age)
         {
-            printf("No valid records found.\n");
+            min_age = std[i].age;
         }
     }
+    min_age=std[i].age;
+    printf("Maximum age is : %d \n", max_age);
+    printf("Minimum age is : %d \n", min_age);
+    // printing the data of max age student.
+    for (i = 0; i < 10; i++){
+        if(max_age == std[i].age){
+            printf("Roll : %d \tName : %s \tAddress : %s \tGender : %c \tAge : %d \n", std[i].roll, std[i].name, std[i].address, std[i].gender, std[i].age);
+        }
+    }
+    // printing min age student detail.
+
+    // Graphics Mode
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "C:\\Turboc3\\BGI");
+    rectangle(50,50,300,150);
     fclose(fp);
     return 0;
 }
